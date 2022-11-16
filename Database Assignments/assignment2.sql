4e4f01db3452c7c41ba02f7c74897366f14919bd
Sushmitha M <55802922+Sushmitha11-M@users.noreply.github.com>
2022-11-16 10:51:17 +0530

    Add files via upload

---
+++ Database Assignments/assignment2.sql
@@ -0,0 +1,17 @@
+create database country;
+
+CREATE TABLE IF NOT EXISTS countries ( 
+COUNTRY_ID varchar(2),
+COUNTRY_NAME varchar(40)
+CHECK(COUNTRY_NAME IN('Italy','India','China')) ,
+REGION_ID decimal(10,0)
+);
+
+select * from countries;
+
+use country;
+
+CREATE TABLE IF NOT EXISTS country_new
+AS SELECT * FROM countries;
+
+SHOW COLUMNS FROM country_new;
